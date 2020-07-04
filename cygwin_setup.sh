106b07c0db94902cda52dfbe71e363e93c811513
furukawa <furukawa@x-ability.co.jp>
2020-07-05 06:27:45 +0900

    remove file list generation

--- cygwin_setup.sh
+++ cygwin_setup.sh
@@ -456,7 +456,4 @@ InstallPhonopy
 InstallMDTraj
 InstallLammps
 
-# Make file list
-find /usr /bin /lib /etc /sbin -type f -print | grep -v -e ':' -e '\\' -e '*' -e '?' -e '<' -e '>' -e '|' -e '"' | sort > /cygdrive/c/cygwin_wm/filelist_cygwinwm.txt
-
 date
